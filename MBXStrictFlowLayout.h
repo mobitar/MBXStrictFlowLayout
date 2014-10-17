51f27d8581cd1e67294419008eb4ec7c9450577b
Mo Bitar <me@bitar.io>
2014-10-17 11:59:12 -0500

    initial commit

---
+++ MBXStrictFlowLayout.h
@@ -0,0 +1,15 @@
+//
+//  MBXStrictFlowLayout.h
+//  ParkWhiz
+//
+//  Created by Mo Bitar on 10/17/14.
+//  Copyright (c) 2014 ParkWhiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBXStrictFlowLayout : UICollectionViewFlowLayout
+
+@property (nonatomic) CGFloat actualItemSpacing;
+
+@end
---
+++ MBXStrictFlowLayout.m
@@ -0,0 +1,35 @@
+//
+//  MBXStrictFlowLayout.m
+//  ParkWhiz
+//
+//  Created by Mo Bitar on 10/17/14.
+//  Copyright (c) 2014 ParkWhiz. All rights reserved.
+//
+
+#import "MBXStrictFlowLayout.h"
+
+@implementation MBXStrictFlowLayout
+
+- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
+{
+    NSArray *answer = [[super layoutAttributesForElementsInRect:rect] mutableCopy];
+    for(int i = 1; i < [answer count]; ++i) {
+        UICollectionViewLayoutAttributes *currentLayoutAttributes = answer[i];
+        UICollectionViewLayoutAttributes *prevLayoutAttributes = answer[i - 1];
+        CGFloat previousMaxX = CGRectGetMaxX(prevLayoutAttributes.frame);
+        CGRect currentFrame = currentLayoutAttributes.frame;
+        CGFloat expectedMaxX = previousMaxX + self.actualItemSpacing + currentLayoutAttributes.frame.size.width;
+        CGFloat difference = self.collectionViewContentSize.width - expectedMaxX;
+        if(difference > 0 || fabs(difference) <= FLOAT_EPSILON) {
+            currentFrame.origin.x = previousMaxX + self.actualItemSpacing;
+            currentFrame.origin.y = CGRectGetMinY(prevLayoutAttributes.frame);
+        } else {
+            currentFrame.origin.x = 0;
+            currentFrame.origin.y = CGRectGetMaxY(prevLayoutAttributes.frame) + self.minimumLineSpacing;
+        }
+        currentLayoutAttributes.frame = currentFrame;
+    }
+    return answer;
+}
+
+@end
