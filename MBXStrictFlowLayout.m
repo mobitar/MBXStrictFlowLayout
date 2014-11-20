65bd5a393fcab97f7101f995a37e7648fdf93dd7
Mo Bitar <me@bitar.io>
2014-11-20 16:34:42 -0600

    updates

--- MBXStrictFlowLayout.m
+++ MBXStrictFlowLayout.m
@@ -15,7 +15,7 @@
 - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
 {
     NSArray *answer = [[super layoutAttributesForElementsInRect:rect] mutableCopy];
-    for(int i = 1; i < [answer count]; ++i) {
+    for(int i = 1; i < [answer count]; i++) {
         UICollectionViewLayoutAttributes *currentLayoutAttributes = answer[i];
         UICollectionViewLayoutAttributes *prevLayoutAttributes = answer[i - 1];
         CGFloat previousMaxX = CGRectGetMaxX(prevLayoutAttributes.frame);
